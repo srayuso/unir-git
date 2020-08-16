303d1a02201bbd86bd26d07b7b5c80836f34df7c
Roberto Santamaria Ayuso <roberto.santamaria@centurylink.com>
2020-08-16 17:09:59 +0200

    mensaje de error con indicacion de duplicados

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ if __name__ == "__main__":
         remove_duplicates = sys.argv[2].lower() == "yes"
     else:
         print("Se debe indicar el fichero como primer argumento")
+        print("El segundo argumento indica si se quieren eliminar duplicados")
         sys.exit(1)
 
     print(f"Se leerán las palabras del fichero {filename}")
