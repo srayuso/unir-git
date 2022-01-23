7ef84942d38ae888e550909637e4e1ff8a351b86
Juanagus <jafraileni@gmail.com>
2022-01-23 09:28:42 -0800

    Nuevo parámetro para indicar el fichero que contiene las palabras

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """
-License: Apache
+License: MIT
 Organization: UNIR
 """
 
@@ -7,8 +7,6 @@ import os
 import sys
 
 DEFAULT_FILENAME = "words.txt"
-DEFAULT_DUPLICATES = False
-
 
 def sort_list(items, ascending=True):
     if not isinstance(items, list):
@@ -16,20 +14,12 @@ def sort_list(items, ascending=True):
 
     return sorted(items, reverse=(not ascending))
 
-
-def remove_duplicates_from_list(items):
-    return list(set(items))
-
-
 if __name__ == "__main__":
     filename = DEFAULT_FILENAME
-    remove_duplicates = DEFAULT_DUPLICATES
-    if len(sys.argv) == 3:
+    if len(sys.argv) == 2:
         filename = sys.argv[1]
-        remove_duplicates = sys.argv[2].lower() == "yes"
     else:
         print("Se debe indicar el fichero como primer argumento")
-        print("El segundo argumento indica si se quieren eliminar duplicados")
         sys.exit(1)
 
     print(f"Se leerán las palabras del fichero {filename}")
@@ -43,7 +33,5 @@ if __name__ == "__main__":
         print(f"El fichero {filename} no existe")
         word_list = ["ravenclaw", "gryffindor", "slytherin", "hufflepuff"]
 
-    if remove_duplicates:
-        word_list = remove_duplicates_from_list(word_list)
-
     print(sort_list(word_list))
+
